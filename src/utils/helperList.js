a5ce24bac4e627e6b0fbfa8a5bbb9e74fb11fa60
nagar <nagar.2@iitj.ac.in>
2021-10-11 00:56:12 +0530

    Initial Commit

---
+++ src/utils/helperList.js
@@ -0,0 +1,127 @@
+export const showcaseSkills = [{
+  label: 'Javascript',
+  rating: 80
+}, {
+  label: 'Node JS',
+  rating: 85
+}, {
+  label: 'React',
+  rating: 90
+}, {
+  label: 'Mongo dB',
+  rating: 70
+}, {
+  label: 'Typescript',
+  rating: 55
+}, {
+  label: 'React Native',
+  rating: 50
+}, {
+  label: 'Ruby on Rails',
+  rating: 60
+}];
+
+// src: 'https://reacttraining.com/images/react-router.svg'
+
+
+export const skillsList = [{
+//   label: 'HTML5',
+//   src: 'https://seeklogo.com/images/H/html5-logo-EF92D240D7-seeklogo.com.png'
+// }, {
+//   label: 'CSS3',
+//   src: 'https://seeklogo.com/images/C/css3-logo-8724075274-seeklogo.com.png'
+// }, {
+  label: 'Javascript',
+  src: 'https://img.icons8.com/color/144/000000/javascript.png',
+}, {
+  label: 'Typescript',
+  src: 'https://img.icons8.com/color/144/000000/typescript.png',
+}, {
+  label: 'Node JS',
+  src: 'https://img.icons8.com/color/144/000000/nodejs.png',
+}, {
+  label: 'React',
+  src: 'https://img.icons8.com/color/144/000000/react-native.png',
+}, {
+  label: 'Express JS',
+  src: 'https://i.ibb.co/9smDM6j/pngegg.png'
+}, {
+  label: 'Mongo dB',
+  src: 'https://i.ibb.co/ZVh01dm/rsz-mong2.png'
+}, {
+  label: 'React Native',
+  src: 'https://i.ibb.co/7NfB2rt/rn2.png',
+}, {
+  label: 'Redux',
+  src: 'https://img.icons8.com/color/144/000000/redux.png',
+}, {
+  label: 'Github',
+  src: 'https://img.icons8.com/fluent/144/000000/github.png'
+}, {
+  label: 'Next JS',
+  src: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg'
+}, {
+  label: 'Jquery',
+  src: 'https://img.icons8.com/ios-filled/144/000000/jquery.png'
+}, {
+  label: 'Ruby',
+  src: 'https://img.icons8.com/color/144/000000/ruby-programming-language.png'
+}, {
+  label: 'Ruby on Rails',
+  src: 'https://rubyonrails.org/images/rails-logo.svg',
+}, {
+  label: 'NPM',
+  src: 'https://img.icons8.com/color/144/000000/npm.png'
+}, {
+  label: 'Yarn',
+  src: 'https://i.ibb.co/GtbG98V/yarn2.png'
+}, {
+  label: 'Material UI',
+  src: 'https://material-ui.com/static/logo_raw.svg'
+}, {
+  label: 'Bootstrap',
+  src: 'https://i.ibb.co/zx0Tt9M/bootstrap.png'
+}, {
+  label: 'Sass',
+  src: 'https://i.ibb.co/zs6B3yR/sass.png'
+}, {
+  label: 'Expo',
+  src: 'https://i.ibb.co/nRSMp2C/icon-7dff78a7e6e913da07dfd995b6ff6edd.png'
+}];
+
+export const projectsList = [{
+  label: 'Auth example - React & Node',
+  imgSrc: 'https://i.ibb.co/fDgXSmh/authex-1.png',
+  src: 'https://auth-example.netlify.app/',
+  githubSrc: 'https://github.com/s8sachin/auth-example-react',
+  githubSrc2: 'https://github.com/s8sachin/auth-example-node'
+}, {
+  label: 'Smitha Karthik Portfolio',
+  imgSrc: 'https://i.ibb.co/94GVFnf/rsz-smit-1.png',
+  src: 'https://smithakarthik.com',
+  githubSrc: 'https://github.com/s8sachin/smitha-karthik'
+}, {
+  label: 'KVHAA - Android app',
+  src: 'https://play.google.com/store/apps/details?id=com.kvhaa.kvhaa',
+  imgSrc: "https://i.ibb.co/wrrvMb2/kvhaapp-1.png"
+}, {
+  label: 'Whatsapp chat parser NPM library',
+  imgSrc: 'https://i.ibb.co/TLVqHMF/rsz-2screenshot-2020-06-28-at-2-53-39-am.png',
+  src: 'https://www.npmjs.com/package/whatsapp-chat-parser-js',
+  githubSrc: 'https://github.com/s8sachin/whatsapp-chat-parser-js',
+  description: 'Simple js library to parse exported whatsapp chat "txt -> json"'
+}, {
+  label: 'Mysore city bus API',
+  imgSrc: 'https://i.ibb.co/LzGmbQK/mcbapp-1.png',
+  src: 'https://play.google.com/store/apps/details?id=com.mysuru_commute',
+  githubSrc: 'https://github.com/s8sachin/mysore_city_bus',
+  description: 'Open API for city bus info in Mysore, this is used in a android app'
+}, {
+  label: 'KVH Alumni Association',
+  imgSrc: 'https://i.ibb.co/r5zJDhc/rsz-kvh-1-1.png',
+  src: 'https://kvhalumni.com/'
+}, {
+  label: 'Mobile shop management app',
+  imgSrc: 'https://i.ibb.co/cCkV7DF/rsz-mobshop-1.png',
+  src: 'https://mobileshopdemo.herokuapp.com/',
+}]
